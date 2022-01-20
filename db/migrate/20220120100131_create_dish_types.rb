70ec3ff60185c40e14db0fc89b4e8e5ea16d3ed3
bozhenaboichuk <vogelweide00@gmail.com>
2022-01-20 12:03:21 +0200

    add DishType model

---
+++ db/migrate/20220120100131_create_dish_types.rb
@@ -0,0 +1,9 @@
+class CreateDishTypes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dish_types do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_094037) do
+ActiveRecord::Schema.define(version: 2022_01_20_100131) do
+
+  create_table "dish_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
