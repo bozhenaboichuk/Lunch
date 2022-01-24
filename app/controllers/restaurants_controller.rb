e203cddc31724d7d56ff14235b414ff28ceb8526
Andriy0 <andriy2152@gmail.com>
2022-01-24 10:09:28 +0200

    Update restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,21 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant!, except: %i[index]
 
   def index
-
+    @restaurants = Restaurant.all
   end
 
+  def show
+    @dishes = @restaurant.dishes.all.includes(:dish_type)
+    @first_dishes = @dishes.where(dish_type: DishType.find_by(name: 'first'))
+    @second_dishes = @dishes.where(dish_type: DishType.find_by(name: 'second'))
+    @salads = @dishes.where(dish_type: DishType.find_by(name: 'salad'))
+    @deserts = @dishes.where(dish_type: DishType.find_by(name: 'desert'))
+  end
+
+  private
+
+  def set_restaurant!
+    @restaurant = Restaurant.find params[:id]
+  end
 end
