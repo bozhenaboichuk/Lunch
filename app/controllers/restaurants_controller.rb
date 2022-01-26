720259b381159a40f5363f9da015e0eb7455992a
bozhenaboichuk <vogelweide00@gmail.com>
2022-01-26 13:43:24 +0200

    Add avatar downloads for Restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -40,6 +40,6 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :phone_number, :address)
+    params.require(:restaurant).permit(:name, :phone_number, :address, :avatar)
   end
 end
