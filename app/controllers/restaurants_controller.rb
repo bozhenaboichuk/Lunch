6dc05670b6d6638f0b9f15b416c0ac58395785b9
bozhenaboichuk <vogelweide00@gmail.com>
2022-01-26 11:47:33 +0200

    Add new Restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,21 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant!, except: %i[index]
+  before_action :set_restaurant!, except: %i[index new create]
 
   def index
     @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new restaurant_params
+      if @restaurant.save
+        redirect_to restaurants_path
+      else
+        render :new
+      end
   end
 
   def show
@@ -17,5 +30,9 @@ class RestaurantsController < ApplicationController
 
   def set_restaurant!
     @restaurant = Restaurant.find params[:id]
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :phone_number, :address)
   end
 end
