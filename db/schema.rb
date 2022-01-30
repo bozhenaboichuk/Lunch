96e62630787bba720cbe548a55aaff6758fdeb99
Andriy0 <andriy2152@gmail.com>
2022-01-30 18:17:29 +0200

    Remove `admin` field from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_160935) do
+ActiveRecord::Schema.define(version: 2022_01_30_161656) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 2022_01_30_160935) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
-    t.boolean "admin", default: false
     t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
