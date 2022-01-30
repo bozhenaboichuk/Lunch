4f2f1f6eaa9efc6ccce71cc63919ba6fd170a313
Andriy0 <andriy2152@gmail.com>
2022-01-30 22:31:18 +0200

    Add GuestUser service
    To avoid errors when user is not signed in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :orders
+
+  def guest?
+    false
+  end
 end
