4fbdaacde7d67903b5e6a71113815ccc13a261fa
bozhenaboichuk <vogelweide00@gmail.com>
2022-01-31 20:10:51 +0200

    New method add dishes with avatar

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,20 +2,27 @@ class DishesController < ApplicationController
   before_action :set_restaurant!
 
   def create
-    @dish = @restaurant.dishes.build dish_params
+      @dish = @restaurant.dishes.build dish_params
+      logger.debug "New dish: #{@dish.attributes.inspect}"
+      logger.debug "Dish should be valid: #{@dish.valid?}"
+      logger.debug @dish.errors.inspect
       if @dish.save
-        redirect_to restaurant_path
+        logger.debug "The dish was saved and now thr user is going to be redirected..."
+        redirect_to restaurant_path(@restaurant), status: :see_other
       else
+        @types = DishType.all
         render 'restaurants/show'
       end
-  end
+    end
+
+  private
 
   def set_restaurant!
     @restaurant = Restaurant.find params[:restaurant_id]
   end
 
   def dish_params
-    params.require(:dish).permit(:price, :weight, :name, :avatar)
+    params.require(:dish).permit(:price, :weight, :name, :avatar, :dish_type_id)
   end
 
 end
--- app/models/dish_type.rb
+++ app/models/dish_type.rb
@@ -1,3 +1,4 @@
 class DishType < ApplicationRecord
   has_many :dishes, dependent: :destroy
+  has_one_attached :avatar
 end
