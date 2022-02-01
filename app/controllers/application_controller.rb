1c32a71c4588cb72c76819f507da56a987ee1ea0
Anzhela09 <koyliak.anzhela.dmutrovna@gmail.com>
2022-02-01 12:45:34 +0200

    update form sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_paramters
         devise_parameter_sanitizer.permit(:account_update, keys: [:first_name,:last_name,:phone_number,:email, :password, :avatar])
     end
+    def configure_permitted_paramters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name,:phone_number,:email, :password])
+    end
 end
