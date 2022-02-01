44a94d666ff6386ff58829aab0a14bff19320fad
Anzhela09 <koyliak.anzhela.dmutrovna@gmail.com>
2022-02-01 21:24:49 +0200

    add for edit user

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,22 @@
+class UserPolicy < ApplicationPolicy
+  def create?
+    user.admin_role?
+  end
+
+  def update?
+    user.admin_role?
+  end
+
+  def destroy?
+    user.admin_role?
+  end
+
+  def index?
+    user.admin_role?
+  end
+
+  def show?
+    true
+  end
+
+end
