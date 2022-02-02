ec7b16986407f45c750535ead61982a8e073ba7a
Andriy0 <andriy2152@gmail.com>
2022-02-02 14:26:38 +0200

    Add validations to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :dishes, dependent: :destroy
   has_one_attached :avatar
+
+  validates :name, length: {minimum: 2}
+  validates :address, length: {minimum: 3}
+  validates :description, length: {minimum: 6}, allow_blank: true
+  validates :phone_number, format: {without: /[a-zA-Z]+/, message: 'does not allow letters'}
 end
