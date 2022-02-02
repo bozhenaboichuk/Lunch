bc2f8f3714513a20bd44af141ecf1806d6fd7ca1
Andriy0 <andriy2152@gmail.com>
2022-02-02 21:41:53 +0200

    Update validations for restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,8 @@ class Restaurant < ApplicationRecord
   has_one_attached :avatar
 
   validates :name, length: {minimum: 2}
-  validates :address, length: {minimum: 3}
+  validates :address, length: {minimum: 3}, allow_blank: true
   validates :description, length: {minimum: 6}, allow_blank: true
-  validates :phone_number, format: {without: /[a-zA-Z]+/, message: 'does not allow letters'}
+  validates :phone_number, format: {without: /[a-zA-Z]+/, message: 'does not allow letters'},
+    length: {minimum: 5}
 end
