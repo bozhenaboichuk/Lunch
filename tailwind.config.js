0bdf8434011b53de8a0e4f9f2332185b72987786
Dufanets <marianadufanets@gmail.com>
2022-02-03 10:07:05 +0200

    update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./app/**/*.{html,js,erb,haml}"],
   theme: {
     extend: {
+      fontFamily: {
+        'nav': ['Didact Gothic'],
+      },
       screens:{
         's1': '450px',
         's2': '640px',
@@ -9,30 +12,13 @@ module.exports = {
         's4': '1024px',
         's5': '1280px',
       },
-      boxShadow:{
-        '3xl': '0 6px 20px -5px rgba(0,0,0,0.4)',
-        '4xl': '0 0 12px -2px rgba(0,0,0,0.5)'
-      },
-      spacing:{
-        '150': '50rem',
-        '120': '664px',
-        '140': '46rem'
-      },
-      strokeWidth:{
-        '3': '3px',
-      },
-      top: {
-       '50': '50%',
-    },
-      left: {
-       '50': '50%',
-  },
-      translate: {
-        '50': '50%',
+       minHeight: {
+         'full': '774px',
+         'users': '710px',
+     },
 },
-},
-variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  variants: {
+
   },
 },
   plugins: [],
