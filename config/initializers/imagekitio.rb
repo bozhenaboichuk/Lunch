920d34eb5d9e3331bfcf324ac40f682059b54bdd
Andrii Maliovanchuk <andriy@Andriis-MacBook-Pro.local>
2022-02-21 18:16:02 +0200

    Fix code style with rubocop

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -4,7 +4,7 @@ ImageKitIo.configure do |config|
   config.public_key = ENV['IMAGEKITIO_PUBLIC_KEY']
   config.private_key = ENV['IMAGEKITIO_PRIVATE_KEY']
   config.url_endpoint = ENV['IMAGEKITIO_ENDPOINT_URL']
-  
+
   config.service = :carrierwave
   # config.service = :active_storage
   # config.constants.MISSING_PRIVATE_KEY = 'custom error message'
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,127 +1,96 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  context 'validations' do
-    context 'name' do
-      it 'has a name' do
-        restaurant = Restaurant.new(
+  context 'during validations' do
+    context 'with name' do
+      let :restaurant do
+        described_class.new(
           name: '',
           address: 'valid address',
           description: 'valid description',
           phone_number: '0985643267'
         )
+      end
 
+      it 'empty' do
         expect(restaurant).not_to be_valid
         restaurant.name = 'test name'
         expect(restaurant).to be_valid
       end
-      
-      it 'has a name minimum 2 symbols' do
-        restaurant = Restaurant.new(
-          name: 't',
-          address: 'valid address',
-          description: 'valid description',
-          phone_number: '0985643267'
-        )
 
+      it 'of minimum 2 symbols' do
         expect(restaurant).not_to be_valid
         restaurant.name = 'va'
         expect(restaurant).to be_valid
       end
     end
 
-    context 'address' do
-      it 'has no address' do
-        restaurant = Restaurant.new(
+    context 'with address' do
+      let :restaurant do
+        described_class.new(
           name: 'valid name',
           address: 'te',
           description: 'valid description',
           phone_number: '0985643267'
         )
+      end
 
-        expect(restaurant).to_not be_valid
+      it 'empty' do
+        expect(restaurant).not_to be_valid
         restaurant.address = ''
         expect(restaurant).to be_valid
       end
-      
-      it 'has an address of minimum 3 symbols' do
-        restaurant = Restaurant.new(
-          name: 'valid name',
-          address: 'te',
-          description: 'valid description',
-          phone_number: '0985643267'
-        )
 
-        expect(restaurant).to_not be_valid
+      it 'of minimum 3 symbols' do
+        expect(restaurant).not_to be_valid
         restaurant.address = 'val'
         expect(restaurant).to be_valid
       end
     end
 
-    context 'description' do
-      it 'has no description' do
-        restaurant = Restaurant.new(
+    context 'with description' do
+      let :restaurant do
+        described_class.new(
           name: 'valid name',
           address: 'valid address',
           description: 'fail',
           phone_number: '0985643267'
         )
+      end
 
-        expect(restaurant).to_not be_valid
+      it 'empty' do
+        expect(restaurant).not_to be_valid
         restaurant.description = ''
         expect(restaurant).to be_valid
       end
-      
-      it 'has a description of minimum 6 symbols' do
-        restaurant = Restaurant.new(
-          name: 'valid name',
-          address: 'valid address',
-          description: 'fail',
-          phone_number: '0985643267'
-        )
 
-        expect(restaurant).to_not be_valid
+      it 'minimum 6 symbols' do
+        expect(restaurant).not_to be_valid
         restaurant.description = 'valid1'
         expect(restaurant).to be_valid
       end
     end
 
-    context 'phone number' do
-      it 'has a phone number' do
-        restaurant = Restaurant.new(
+    context 'with phone number' do
+      let :restaurant do
+        described_class.new(
           name: 'valid name',
           address: 'valid address',
           description: 'valid description',
           phone_number: ''
         )
-
-        expect(restaurant).to_not be_valid
-        restaurant.phone_number = '0875643278'
-        expect(restaurant).to be_valid
       end
-      
-      it 'has a phone number of minimum 5 symbols' do
-        restaurant = Restaurant.new(
-          name: 'valid name',
-          address: 'valid address',
-          description: 'valid description',
-          phone_number: '0985'
-        )
 
-        expect(restaurant).to_not be_valid
+      it 'of minimum 5 symbols' do
+        expect(restaurant).not_to be_valid
         restaurant.phone_number = '09856'
         expect(restaurant).to be_valid
       end
-      
-      it 'has a phone number without alphabetic characters' do
-        restaurant = Restaurant.new(
-          name: 'valid name',
-          address: 'valid address',
-          description: 'valid description',
-          phone_number: '0985abdv'
-        )
 
-        expect(restaurant).to_not be_valid
+      it 'without alphabetic characters' do
+        expect(restaurant).not_to be_valid
         restaurant.phone_number = '0985678'
         expect(restaurant).to be_valid
       end
