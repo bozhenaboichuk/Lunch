971a5f342ca56b0d71608ba6ec1625ce94c097aa
Andriy0 <andriy2152@gmail.com>
2022-02-22 23:58:42 +0200

    Change 'context' to 'describe'

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  context 'during validations' do
-    context 'with name' do
+  describe 'attribute validation' do
+    describe 'name validations' do
       let :restaurant do
         described_class.new(
           name: '',
@@ -14,20 +14,20 @@ RSpec.describe Restaurant, type: :model do
         )
       end
 
-      it 'empty' do
+      it 'has a name' do
         expect(restaurant).not_to be_valid
         restaurant.name = 'test name'
         expect(restaurant).to be_valid
       end
 
-      it 'of minimum 2 symbols' do
+      it 'has a name of minimum 2 symbols' do
         expect(restaurant).not_to be_valid
         restaurant.name = 'va'
         expect(restaurant).to be_valid
       end
     end
 
-    context 'with address' do
+    describe 'address validations' do
       let :restaurant do
         described_class.new(
           name: 'valid name',
@@ -37,20 +37,20 @@ RSpec.describe Restaurant, type: :model do
         )
       end
 
-      it 'empty' do
+      it 'has no address' do
         expect(restaurant).not_to be_valid
         restaurant.address = ''
         expect(restaurant).to be_valid
       end
 
-      it 'of minimum 3 symbols' do
+      it 'has an address of minimum 3 symbols' do
         expect(restaurant).not_to be_valid
         restaurant.address = 'val'
         expect(restaurant).to be_valid
       end
     end
 
-    context 'with description' do
+    describe 'description validations' do
       let :restaurant do
         described_class.new(
           name: 'valid name',
@@ -60,20 +60,20 @@ RSpec.describe Restaurant, type: :model do
         )
       end
 
-      it 'empty' do
+      it 'has no description' do
         expect(restaurant).not_to be_valid
         restaurant.description = ''
         expect(restaurant).to be_valid
       end
 
-      it 'minimum 6 symbols' do
+      it 'has a description minimum 6 symbols' do
         expect(restaurant).not_to be_valid
         restaurant.description = 'valid1'
         expect(restaurant).to be_valid
       end
     end
 
-    context 'with phone number' do
+    describe 'phone number validations' do
       let :restaurant do
         described_class.new(
           name: 'valid name',
@@ -83,13 +83,13 @@ RSpec.describe Restaurant, type: :model do
         )
       end
 
-      it 'of minimum 5 symbols' do
+      it 'has a phone number of minimum 5 symbols' do
         expect(restaurant).not_to be_valid
         restaurant.phone_number = '09856'
         expect(restaurant).to be_valid
       end
 
-      it 'without alphabetic characters' do
+      it 'has a phone number without alphabetic characters' do
         expect(restaurant).not_to be_valid
         restaurant.phone_number = '0985678'
         expect(restaurant).to be_valid
