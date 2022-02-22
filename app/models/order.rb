0a3b4377e1a9baeb0bc2b4bfbe2862b971cace9d
Andrii Maliovanchuk <andriy@Andriis-MacBook-Pro.local>
2022-02-22 17:32:48 +0200

    Remove last 'order' method call
    For scope 'all_or_today' to work correctly.
    Fixed code style with rubocop.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,12 +5,12 @@ class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_many :dishes, through: :line_items
 
-  scope :all_or_today, ->(opt) do
+  scope :all_or_today, -> lambda { |opt|
     orders = includes(:user).where(submitted: true)
 
-    orders.where('DATE(created_at) = ?',
-                 Time.now.strftime('%Y-%m-%d')) unless opt == :all
-    
-    orders.order(created_at: :desc)
-  end
+    unless opt&.to_sym == :all
+      orders.where('DATE(created_at) = ?',
+                   Time.zone.now.strftime('%Y-%m-%d'))
+    end
+  }
 end
