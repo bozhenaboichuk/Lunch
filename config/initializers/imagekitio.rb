10d7daa31b1a710a96ef1d7dcf94f1b9c11c6065
Andriy0 <56863249+Andriy0@users.noreply.github.com>
2022-02-23 12:41:56 +0200

    Feature rspec (#39)

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 ImageKitIo.configure do |config|
-  if Rails.env.development? || Rails.env.production?
-    config.public_key = ENV['IMAGEKITIO_PUBLIC_KEY']
-    config.private_key = ENV['IMAGEKITIO_PRIVATE_KEY']
-    config.url_endpoint = ENV['IMAGEKITIO_ENDPOINT_URL']
-  end
+  config.public_key = ENV['IMAGEKITIO_PUBLIC_KEY']
+  config.private_key = ENV['IMAGEKITIO_PRIVATE_KEY']
+  config.url_endpoint = ENV['IMAGEKITIO_ENDPOINT_URL']
+
   config.service = :carrierwave
   # config.service = :active_storage
   # config.constants.MISSING_PRIVATE_KEY = 'custom error message'
