a2ff2b39886c9c176f81558b7c213d7eaee83fbd
Andriy0 <andriy2152@gmail.com>
2022-06-06 14:24:03 +0300

    Add usage of `secrets.yml`

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 ImageKitIo.configure do |config|
-  config.public_key = ENV['IMAGEKITIO_PUBLIC_KEY']
-  config.private_key = ENV['IMAGEKITIO_PRIVATE_KEY']
-  config.url_endpoint = ENV['IMAGEKITIO_ENDPOINT_URL']
+  imagekitio_secrets = Rails.application.secrets.imagekitio
+  config.public_key = ENV['IMAGEKITIO_PUBLIC_KEY'] || imagekitio_secrets[:public_key]
+  config.private_key = ENV['IMAGEKITIO_PRIVATE_KEY'] || imagekitio_secrets[:private_key]
+  config.url_endpoint = ENV['IMAGEKITIO_ENDPOINT_URL'] || imagekitio_secrets[:url_endpoint]
 
   config.service = :carrierwave
   # config.service = :active_storage
